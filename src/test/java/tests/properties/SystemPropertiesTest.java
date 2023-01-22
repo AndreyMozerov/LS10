a43e1d9ded00fb304d0f95ce450442a9ccad6e48
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-01-22 12:55:02 +0300

    part2 v2 properties

--- src/test/java/tests/properties/SystemPropertiesTest.java
+++ src/test/java/tests/properties/SystemPropertiesTest.java
@@ -1,4 +1,51 @@
 package tests.properties;
 
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 public class SystemPropertiesTest {
+    @Test
+    void simplePropertyTest() {
+        String browserName = System.getProperty("browser");
+        System.out.println(browserName); // null
+    }
+
+    @Test
+    void simpleProperty1Test() {
+        System.setProperty("browser", "opera");
+        String browserName = System.getProperty("browser");
+        System.out.println(browserName); // opera
+    }
+
+    @Test
+    void simpleProperty2Test() {
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName); // firefox
+    }
+
+    @Test
+    void simpleProperty3Test() {
+        System.setProperty("browser", "opera");
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName); // opera
+    }
+
+    @Test
+    @Tag("one_property")
+    void simpleProperty4Test() {
+        String browserName = System.getProperty("browser", "firefox");
+        System.out.println(browserName);
+        // gradle clean one_property_test
+        // firefox
+
+        // gradle clean one_property_test -Dbrowser=safari
+        // safari
+    }
+
+    @Test
+    @Tag("hello")
+    void simpleProperty5Test() {
+        System.out.println("Hello, " + System.getProperty("user_name", "unknown student"));
+
+    }
 }
